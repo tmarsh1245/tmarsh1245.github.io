7e76b4c700e570500b749e183fc51cace35e73a3
tmarsh1245 <tmarsh@umass.edu>
2018-11-17 11:47:09 -0500

    Work

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import {NavBar} from "./Components/NavBar";
 import {HomePage} from "./HomePage";
 export const App = () => (
+
     <div id="app-entry">
+        Hey
         <NavBar/>
         <HomePage/>
     </div>
